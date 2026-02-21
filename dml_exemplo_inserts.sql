a1db2ca76e4387d1f50314aaba90b69aee6e19e6
paulabortiz <paula.barrosortiz@gmail.com>
2026-02-20 22:49:05 -0300

    Exemplo de INSERT

---
+++ dml_exemplo_inserts.sql
@@ -0,0 +1,33 @@
+
+INSERT INTO stg_escolas_2024 (
+    no_regiao,
+    sg_uf,
+    no_municipio,
+    tp_dependencia,
+    tp_localizacao,
+    in_agua_rede_publica,
+    in_energia_rede_publica,
+    in_esgoto_rede_publica,
+    in_internet,
+    in_banda_larga,
+    in_laboratorio_informatica,
+    in_biblioteca,
+    in_quadra_esportes,
+    qt_salas_utilizadas
+)
+VALUES (
+    'Sul',
+    'RS',
+    'Porto Alegre',
+    2,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    15
+);
